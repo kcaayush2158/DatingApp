de325984ff292f9de2bd65aac71063800dcc4ad9
System Administrator <aayushkc2158@gmail.com>
2021-07-02 20:09:58 +0545

    bug fixes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from '../user';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpClientService } from '../http-client.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import {  FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { LocalStorageService } from 'ngx-webstorage';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { AppService } from '../app.service';
@@ -13,34 +13,40 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  private loggedIn :boolean;
+  private loggedIn: boolean;
   users = User;
   messageResponse = '';
-  credentials = {email: '', password: ''};
+  credentials = { email: '', password: '' };
   recoveryemail: string;
-  loginForm :  FormGroup;
-  authenciate =false;
-  onlineUsers=  [];
-  baseurl='https://lovecupid.herokuapp.com/api';
+  loginForm: FormGroup;
+  authenciate = false;
+  onlineUsers = [];
+  baseurl = 'https://lovecupid.herokuapp.com/api';
 
+  constructor(
+    private app: AppService,
+    private spinner: NgxSpinnerService,
+    private toastr: ToastrService,
+    private activatedRoute: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService,
+    private _httpClient: HttpClient,
+    private service: HttpClientService,
+    private localStorage: LocalStorageService
+  ) {}
 
-
-constructor(private app: AppService,private spinner:NgxSpinnerService,private toastr :ToastrService ,private activatedRoute :ActivatedRoute ,private _router :Router,private _userService: UserService,private _httpClient : HttpClient,private service:HttpClientService,private localStorage:LocalStorageService) { }
-
-
-ngOnInit(): void {
-  this.spinner.show();
-  if(this.localStorage.retrieve('isLoggedIn') == true){
-    this.activatedRoute.params.subscribe((data)=>{
-      this.spinner.hide();
-      this.toastr.success('success', 'Login Successful');
-      this._router.navigateByUrl['/home'];
-    }); 
-  }
+  ngOnInit(): void {
+    this.spinner.show();
+    if (this.localStorage.retrieve('isLoggedIn') == true) {
+      this.activatedRoute.params.subscribe(data => {
+        this.spinner.hide();
+        this.toastr.success('success', 'Login Successful');
+        this._router.navigateByUrl['/home'];
+      });
+    }
   }
 
   authenticated() {
@@ -48,62 +54,48 @@ ngOnInit(): void {
   }
 
   login() {
-   const params = new HttpParams();
-   params.set('email',this.credentials.email)
-   params.set('password',this.credentials.password);
+    const params = new HttpParams();
+    params.set('email', this.credentials.email);
+    params.set('password', this.credentials.password);
 
-    const url=this.baseurl+"/user/login?email="+this.credentials.email+"&password="+this.credentials.password;
-    this._httpClient.get(url).subscribe((response) => {
-    
+    const url = this.baseurl + '/user/login?email=' + this.credentials.email + '&password=' + this.credentials.password;
+    this._httpClient.get(url).subscribe(
+      response => {
         if (response) {
-          this.localStorage.store('user',response);
-          this.localStorage.store("isLoggedIn", "true");
-            this.authenciate = true;
-            this.messageResponse ='success';
-            this._router.navigateByUrl('/home');
-            this.toastr.success('success','Login successful');
-            this.onlineUsers.push(response);
-           
-          
+          this.localStorage.store('user', response);
+          this.localStorage.store('isLoggedIn', 'true');
+          this.authenciate = true;
+          this.messageResponse = 'success';
+          this._router.navigateByUrl('/home');
+          this.toastr.success('success', 'Login successful');
+          this.onlineUsers.push(response);
         } else {
           this._router.navigateByUrl('/login');
           this.messageResponse = 'Bad credentials';
-          this.authenciate=false;
-
+          this.authenciate = false;
         }
-    
-  },(error)=>   this.toastr.error('error', 'invalid credentials')
+      },
+      error => this.toastr.error('error', 'invalid credentials')
 
+      // // for social-login
+      // facebookLogin(){
+      //   this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID).then((userData)=> {
+      //     this.user = userData;
+      //   });
+      // }
+      // googleLogin(){
+      //   this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID).then((userData)=> {
+      //     this.user = userData;
+      //   });
+      // }
 
-  
-
-  
-    // // for social-login
-    // facebookLogin(){
-    //   this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID).then((userData)=> {
-    //     this.user = userData;
-    //   });
-    // }
-    // googleLogin(){
-    //   this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID).then((userData)=> {
-    //     this.user = userData;
-    //   });
-    // }
-
-    // signOut(): void {
-    //   this.socialAuthService.signOut();
-    //   this.localStorage.clear('user');
-    // }
-
-
-
-
-
-
-
-    )
+      // signOut(): void {
+      //   this.socialAuthService.signOut();
+      //   this.localStorage.clear('user');
+      // }
+    );
+  }
 }
-} 
 
 function authenticated(): (error: any) => void {
   throw new Error('Function not implemented.');
--- src/app/login/recover/recover.component.ts
+++ src/app/login/recover/recover.component.ts
@@ -8,35 +8,38 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-recover',
   templateUrl: './recover.component.html',
-  styleUrls: ['./recover.component.scss']
+  styleUrls: ['./recover.component.scss'],
 })
 export class RecoverComponent implements OnInit {
-
-  baseurl='https://lovecupid.herokuapp.com/api';
+  baseurl = 'https://lovecupid.herokuapp.com/api';
   public retypePassword;
   public newPasword;
-  passwordForm:FormGroup;
-  constructor(private toastr:ToastrService,private spinner :NgxSpinnerService,private router:Router,private httpClient :HttpClient,private formBuilder :FormBuilder) { }
+  passwordForm: FormGroup;
+  constructor(
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService,
+    private router: Router,
+    private httpClient: HttpClient,
+    private formBuilder: FormBuilder
+  ) {}
 
-  get changePassword(){
+  get changePassword() {
     return this.passwordForm.controls;
   }
 
   ngOnInit(): void {
-
     this.passwordForm = new FormGroup({
-      newPassword :new FormControl(['',Validators.required,Validators.minLength(6),Validators.minLength(15) ]),
-      password:new FormControl(['',Validators.required,Validators.minLength(6),Validators.minLength(15) ])
-    })
+      newPassword: new FormControl(['', Validators.required, Validators.minLength(6), Validators.minLength(15)]),
+      password: new FormControl(['', Validators.required, Validators.minLength(6), Validators.minLength(15)]),
+    });
   }
 
-  recoverAccount(){
+  recoverAccount() {
     this.spinner.show();
-    const url = this.baseurl+'/recover';
-    this.httpClient.post(url,{}).subscribe((data)=>{
+    const url = this.baseurl + '/recover';
+    this.httpClient.post(url, {}).subscribe(data => {
       this.spinner.hide();
-      this.toastr.show('success','Please check your email');
-    })
+      this.toastr.show('success', 'Please check your email');
+    });
   }
-
 }
